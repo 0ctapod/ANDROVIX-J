d2040d064ed846a77832c2b5e0afe6e3494f12df
Javier Pruebas <moxiyoshida@gmail.com>
2026-04-22 19:46:58 -0500

    Agregando la vista del Dashboard

---
+++ Dashboard/js/main.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', () =>
+{
+  loadComponent('nav', 'nav');
+  loadComponent('sidebar', 'sidebar');
+  loadComponent('sidebar-mobile', 'sidebar-mobile');
+  loadComponent('main', 'main');
+});
